decaccda0a976048ebf2499603d100a3a2c1b2b0
siluye <seife@yekicomp.com>
2022-12-18 01:20:57 -0800

    Push & Pall

---
+++ mont_puspal.c
@@ -0,0 +1,57 @@
+#include "monty.h"
+
+/**
+ * push - push an integer onto the stack
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: script line number
+ *
+ * Return: void
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *arg;
+	int n;
+
+	arg = strtok(NULL, "\n\t\r ");
+	if (arg == NULL || check_for_digit(arg))
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: usage: push integer\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(arg);
+	if (!add_node(stack, n))
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	var.stack_len++;
+}
+
+/**
+ * pall - print all values on `stack' starting from the top
+ * @stack: double pointer to head of stack
+ * @line_number: line number being executed from script file
+ *
+ * Return: void
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head;
+
+	(void)(line_number);
+
+	head = *stack;
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+		if (head == *stack)
+		{
+			return;
+		}
+	}
+}
